13ff969ab4d833449f8a78c55558a723b143c9a9
Adi Wahyudi <adi.whyd02@gmail.com>
2024-08-12 23:36:58 +0700

    feat: add public apibase

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,11 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
+  runtimeConfig: {
+    public: {
+      apiBase: "https://ecommerce-app-api-prod.vercel.app",
+    },
+  },
   vite: {
     css: {
       preprocessorOptions: {
