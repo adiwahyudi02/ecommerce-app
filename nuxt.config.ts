47bfc102b50d703db8480d086babfd96c0375d2c
Adi Wahyudi <adi.whyd02@gmail.com>
2024-08-12 21:58:34 +0700

    chore: add @nuxt/image dependency

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -24,5 +24,5 @@ export default defineNuxtConfig({
     // It's important that this comes last if you have overrides you wish to apply
     "~/components",
   ],
-  modules: ["@nuxt/icon"],
+  modules: ["@nuxt/icon", "@nuxt/image"],
 });
